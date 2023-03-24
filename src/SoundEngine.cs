f9a46cd0df08faa9957716868a26ab16488ed41e
Aniveal <timo.loher@hotmail.com>
2023-03-24 17:34:02 +0100

    Added SoundEngine Class

---
+++ src/SoundEngine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meridian2
+{
+    public class SoundEngine
+    {
+        private static SoundEngine instance = new SoundEngine();
+
+        private SoundEngine() {
+            
+        }
+
+        //Use this method to get a ref to the SoundEngine object
+        public static SoundEngine Instance() { 
+            return instance; 
+        }
+    }
+}
