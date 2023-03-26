ba10e49b04e88729c669af84f7f15a00462e34b0
Jules Bachmann <julesb@student.ethz.ch>
2023-03-26 17:37:29 +0200

    Added electrical columns, electrify mechanic to rpeSegments, and pointers to neighbors of RopeSegments

---
+++ src/ElectricColumn.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tainicom.Aether.Physics2D.Dynamics;
+
+namespace Meridian2
+{
+    internal class ElectricColumn : ActivableColumn
+    {
+        public ElectricColumn(RopeGame game, World world, Vector2 center, float radius, Texture2D texture) : base(game, world, center, radius, texture)
+        {
+
+        }
+    }
+}
--- src/RopeSegment.cs
+++ src/RopeSegment.cs
@@ -11,12 +11,20 @@ public class RopeSegment : IGameObject {
     private readonly GameScreen _gameScreen;
     private readonly Vector2 _position;
     private readonly Vector2 _size;
+    public RopeSegment previous = null; //may be null if none
+    public RopeSegment next = null; //may be null if none
 
     public Body Body;
 
     private const float RopeDensity = 0.01f;
 
     private bool _black;
+
+    private bool _elecSrc = false;
+    private const int _elecRange = 10; //range in segments
+    public int elecIntensity = 0;
+    public bool elecFromPrev = false;
+    public bool elecFromNext = false;
 
     public RopeSegment(Rope rope, GameScreen gameScreen, Vector2 position, Vector2 size) {
         _rope = rope;
@@ -38,6 +46,116 @@ public class RopeSegment : IGameObject {
         }
     }
 
+    public void SetPrevious(RopeSegment previous)
+    {
+        this.previous = previous;
+    }
+
+    public void SetNext(RopeSegment next) 
+    { 
+        this.next = next;
+    }
+
+    public void ElectrifyByPrevious(int intensity)
+    {
+        if (_elecSrc)
+        {
+            return;
+        }
+        if (elecFromNext)
+        {
+            if (intensity > elecIntensity)
+            {
+                elecIntensity = intensity;
+                elecFromNext = false;
+                elecFromPrev = true;
+                next.ElectrifyByPrevious(intensity - 1);
+            }
+            return;
+        }
+        if (intensity > elecIntensity)
+        {
+            elecIntensity = intensity;
+            elecFromPrev = true;
+            next?.ElectrifyByPrevious(intensity - 1);
+            return;
+        }
+        if (elecFromPrev && intensity < elecIntensity)
+        {
+            elecIntensity = intensity;
+            if (next != null)
+            {
+                if (next.elecFromNext && next.elecIntensity - 1 > intensity) 
+                {
+                    elecIntensity = next.elecIntensity - 1;
+                    elecFromNext = true;
+                    elecFromPrev = false;
+                    previous.ElectrifyByNext(elecIntensity - 1);
+                    return;
+                }
+                if (intensity > 0)
+                {
+                    next.ElectrifyByPrevious(intensity - 1);
+                }
+                else
+                {
+                    elecFromPrev = false;
+                    next.ElectrifyByPrevious(0);
+                }
+            }
+        }
+    }
+
+    public void ElectrifyByNext(int intensity)
+    {
+        if (_elecSrc)
+        {
+            return;
+        }
+        if (elecFromPrev)
+        {
+            if (intensity > elecIntensity)
+            {
+                elecIntensity = intensity;
+                elecFromPrev = false;
+                elecFromNext = true;
+                previous.ElectrifyByNext(intensity - 1);
+            }
+            return;
+        }
+        if (intensity > elecIntensity)
+        {
+            elecIntensity = intensity;
+            elecFromNext = true;
+            previous?.ElectrifyByNext(intensity - 1);
+            return;
+        }
+        if (elecFromNext && intensity < elecIntensity)
+        {
+            elecIntensity = intensity;
+            if (previous != null)
+            {
+                if (previous.elecFromPrev && previous.elecIntensity - 1 > intensity)
+                {
+                    elecIntensity = previous.elecIntensity - 1;
+                    elecFromPrev = true;
+                    elecFromNext = false;
+                    next.ElectrifyByNext(elecIntensity - 1);
+                    return;
+                }
+                if (intensity > 0)
+                {
+                    previous.ElectrifyByPrevious(intensity - 1);
+                }
+                else
+                {
+                    elecFromNext = false;
+                    previous.ElectrifyByPrevious(0);
+                }
+            }
+        }
+    }
+
     public void LoadContent() {
         // Nothing to load
     }
@@ -47,6 +165,12 @@ public class RopeSegment : IGameObject {
     }
 
     public void Draw(GameTime gameTime, SpriteBatch batch) {
+        if (elecIntensity > 0)
+        {
+            batch.Draw(_rope.BaseTexture, sourceRectangle: null, position: Body.Position, scale: 1f, rotation: Body.Rotation,
+            color: Color.Yellow, origin: Vector2.Zero, effects: SpriteEffects.None, layerDepth: 0f);
+            return;
+        }
         if (_black)
             {
             batch.Draw(_rope.BaseTexture, sourceRectangle: null, position: Body.Position, scale: 1f, rotation: Body.Rotation,
@@ -78,6 +202,36 @@ public class RopeSegment : IGameObject {
                 //TODO: change this when changing unique trigger
                 _rope._fragiles.RemoveAll(x => x == column);
             }
+        }
+        if (column is ElectricColumn)
+        {
+            if (collision)
+            {
+                _elecSrc = true;
+                elecFromNext = false;
+                elecFromPrev = false;
+                elecIntensity = _elecRange;
+                next?.ElectrifyByPrevious(elecIntensity - 1);
+                previous?.ElectrifyByNext(elecIntensity - 1);
+            }
+            else
+            {
+                _elecSrc = false;
+                if (next != null && (next._elecSrc | next.elecFromNext))
+                {
+                    elecIntensity = next.elecIntensity - 1;
+                    previous?.ElectrifyByNext(next.elecIntensity);
+                } else if (previous != null && (previous._elecSrc | previous.elecFromPrev))
+                {
+                    elecIntensity = previous.elecIntensity - 1;
+                    next?.ElectrifyByPrevious(previous.elecIntensity);
+                } else
+                {
+                    elecIntensity = 0;
+                    next?.ElectrifyByPrevious(0);
+                    previous?.ElectrifyByNext(0);
+                }
+            }            
         }
     }
 }
