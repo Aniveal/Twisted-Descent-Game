726028ed17e3dc31ae00151b8922c8edb6663eb4
Jules Bachmann <julesb@student.ethz.ch>
2023-04-30 16:21:52 +0200

    added new sprite to treasure chest, untested

--- src/Treasures/Chest.cs
+++ src/Treasures/Chest.cs
@@ -10,13 +10,14 @@ namespace Meridian2.Treasures;
 public class Chest : IDrawableObject {
     public Body Body;
     protected RopeGame Game;
-    protected float Height = 0.5f; //default height, sprite size hardcoded based on this
 
     public bool Open = false;
     public Vector2 Pos;
     protected Texture2D TextureClosed;
     protected Texture2D TextureOpen;
-    protected float Width = 1; //default width, sprite size hardcoded based on this
+    protected float Width = 0.75f;
+    protected float Height = 0.5f;
+    protected float spriteWidth = 0.91f;
     protected World World;
 
 
@@ -25,12 +26,21 @@ public class Chest : IDrawableObject {
         Game = game;
         World = world;
         Pos = position;
-        Body = World.CreateRectangle(Width, Height, 1, Pos, -MathHelper.Pi / 4);
+        Body = World.CreateRectangle(Width, Height, 0, Pos, MathHelper.Pi / 4);
+        Body.Tag = this;
     }
+
+    public void LoadContent() {
+        TextureClosed = Game.Content.Load<Texture2D>("Chest/chest_closed");
+        TextureOpen = Game.Content.Load<Texture2D>("Chest/chest_open");
+    }
+
+    //Overwrite in subclasses
+    public virtual void Loot() { }
 
     public void Draw(GameTime gameTime, SpriteBatch batch, Camera camera) {
         var dstRec = new Rectangle();
-        dstRec = camera.getScreenRectangle(Pos.X - 0.3536f, Pos.Y - 0.0701f, 2.0607f, 1.5303f);
+        dstRec = camera.getSpriteRectangle(Pos.X - spriteWidth*0.5f, Pos.Y+Height, spriteWidth, 1.31f*spriteWidth);
         if (Open)
             batch.Draw(TextureOpen, dstRec, null, Color.White, 0f, Vector2.Zero, SpriteEffects.None,
                 camera.getLayerDepth(dstRec.Y + dstRec.Height));
@@ -38,13 +48,6 @@ public class Chest : IDrawableObject {
             batch.Draw(TextureClosed, dstRec, null, Color.Red, 0f, Vector2.Zero, SpriteEffects.None,
                 camera.getLayerDepth(dstRec.Y + dstRec.Height));
     }
-
-    public void LoadContent() {
-        TextureClosed = Game.Content.Load<Texture2D>("rectangle");
-        TextureOpen = Game.Content.Load<Texture2D>("rectangle");
-    }
-
-    public virtual void Loot() { }
 
     protected bool OnCollision(Fixture sender, Fixture other, Contact contact) {
         Body collider;
@@ -57,6 +60,7 @@ public class Chest : IDrawableObject {
         if (collider.Tag is Player)
             //TODO: play opening animation?
             Loot();
+            Open = true;
         return true;
     }
 }
