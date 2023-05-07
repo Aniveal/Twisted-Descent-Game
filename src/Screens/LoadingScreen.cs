ad8f52689bf08009e0f2cd0cfcb13fb9cecef801
AradM <arad@DESKTOP-MI04IVJ.localdomain>
2023-05-07 10:39:42 +0200

    change tutorial and loading screen, make them dynamic

--- src/Screens/LoadingScreen.cs
+++ src/Screens/LoadingScreen.cs
@@ -15,17 +15,31 @@ public class LoadingScreen : Screen {
     public GraphicsDeviceManager Graphics;
     private Texture2D _playerModel;
     private Texture2D _enemyModel;
+
+    private Texture2D _menu_img;
+    private Texture2D _menu_title;
     private Texture2D _bg;
+
     public Boolean gameLoaded;
     double timer;
     private SpriteFont font;
 
+    private int w;
+    private int h;
+
     public LoadingScreen(RopeGame game, ContentManager content) : base(game)
     {
-        font = content.Load<SpriteFont>("Arial40");
+        w = game.Graphics.PreferredBackBufferWidth;
+        h = game.Graphics.PreferredBackBufferHeight;
+
+        font = content.Load<SpriteFont>("Damn");
         _playerModel = content.Load<Texture2D>("Sprites/Theseus/model");
         _enemyModel = content.Load<Texture2D>("Sprites/Enemies/Minotaur/minotaur_idle_flip");
-        _bg = content.Load<Texture2D>("Sprites/bg");
+
+        _bg = content.Load<Texture2D>("Sprites/UI/menu_background");
+        _menu_img = content.Load<Texture2D>("Sprites/UI/menu_img");
+        _menu_title = content.Load<Texture2D>("Sprites/UI/menu_title");
+
         gameLoaded = false;
         timer = 0;
 
@@ -34,11 +48,17 @@ public class LoadingScreen : Screen {
     public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
     {
         spriteBatch.Begin();
-        spriteBatch.Draw(_bg, new Rectangle(0, 0, 1600, 900), Color.White);
-        spriteBatch.Draw(_playerModel, new Rectangle(1190, 100, 400, 700), Color.White);
-        spriteBatch.Draw(_enemyModel, new Rectangle(10, 100, 400, 700), Color.White);
-        spriteBatch.DrawString(font, "Generating The Level", new Vector2(560, 200), Color.White);
-        spriteBatch.DrawString(font, "This Might Take Couple Of Seconds", new Vector2(400, 320), Color.White);
+        spriteBatch.Draw(_bg, new Rectangle(0, 0, w, h), Color.White);
+        int title_width = Math.Min(w / 2 - 150, 2 * (h - 200) / 3);
+        int title_height = 2 * title_width / 3;
+        spriteBatch.Draw(_menu_title, new Rectangle(100, h - title_height - 100, title_width, title_height), Color.White);
+        int img_width = Math.Min(w / 2 - 150, (h - 200) / 2);
+        int img_height = 2 * img_width;
+        spriteBatch.Draw(_menu_img, new Rectangle(w - 100 - img_width, h - 100 - img_height, img_width, img_height), Color.White);
+        //spriteBatch.Draw(_playerModel, new Rectangle(w / 4 * 3 - 10, h / 9, w / 4, h / 9 * 7), Color.White);
+        //spriteBatch.Draw(_enemyModel, new Rectangle(10, h / 9, w / 4, h / 9 * 7), Color.White);
+        spriteBatch.DrawString(font, "Generating The Level", new Vector2(w / 2 - 220, h / 9 * 2), Color.White);
+        spriteBatch.DrawString(font, "This Might Take Couple Of Seconds", new Vector2(w / 2 - 350, h / 9 * 3), Color.White);
         spriteBatch.End();
     }
 
