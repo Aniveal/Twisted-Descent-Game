eb26bda675194a66f3b7d5d446da6f2609acb2b2
bglaus <bglaus@student.ethz.ch>
2023-05-10 14:15:16 +0200

    changed health and kill count UI

--- src/Gui/StatsGui.cs
+++ src/Gui/StatsGui.cs
@@ -1,41 +1,85 @@
-﻿using Meridian2.GameElements;
+﻿using System;
+using System.Diagnostics;
+using Meridian2.GameElements;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace Meridian2.Gui; 
+namespace Meridian2.Gui;
 
-public class StatsGui : DrawableGameElement {
+public class StatsGui : DrawableGameElement
+{
     private readonly GameData _data;
     private readonly RopeGame _game;
 
     private Texture2D _enemyTexture;
+    private Texture2D _skull;
+    private Texture2D _skull_1;
+    private Texture2D _skull_2;
     private SpriteFont _font;
 
-    public StatsGui(RopeGame game, GameData data) {
+    private int max_skulls;
+    private int skull_size;
+
+    public StatsGui(RopeGame game, GameData data)
+    {
         _game = game;
         _data = data;
-    }
 
-    public void LoadContent() {
-        _enemyTexture = _game.Content.Load<Texture2D>("Sprites/Enemies/enemy");
-        _font = _game.Content.Load<SpriteFont>("Damn");
-    }
+        skull_size = 42 + 5;
 
-    public override void Draw(GameTime gameTime, SpriteBatch batch, Camera camera) {
+        // maximum number of skulls shown depends on the width of the screen
         var viewportWidth = _game.GraphicsDevice.Viewport.Width;
+        max_skulls = (int)((viewportWidth / 2) / (5 * skull_size / 8));
 
+    }
+
+    public void LoadContent()
+    {
+        _enemyTexture = _game.Content.Load<Texture2D>("Sprites/Enemies/enemy");
+        _skull = _game.Content.Load<Texture2D>("Sprites/UI/skull_icon");
+        _skull_1 = _game.Content.Load<Texture2D>("Sprites/UI/skull_icon_1");
+        _skull_2 = _game.Content.Load<Texture2D>("Sprites/UI/skull_icon_2");
+        _font = _game.Content.Load<SpriteFont>("damn_ui");
+    }
+
+    public override void Draw(GameTime gameTime, SpriteBatch batch, Camera camera)
+    {
+        var viewportWidth = _game.GraphicsDevice.Viewport.Width;
+        var margin = 10;
+
+        // Writing the number of Skills as a number
         var text = _data.Kills.ToString();
         var stringSize = _font.MeasureString(_data.Kills.ToString());
-        var margin = 10;
-        var enemyWidth = (int)(((float)_enemyTexture.Width / (float)_enemyTexture.Height) * stringSize.Y);
-        var enemyTextureRect = new Rectangle(viewportWidth - margin - enemyWidth, -8, enemyWidth, (int)stringSize.Y);
-        var position = new Vector2(viewportWidth - stringSize.X - 2*margin - enemyTextureRect.Width, margin);
-        
-        batch.DrawString(_font, text, position, new(170, 54, 54), 0, Vector2.Zero, 1f, SpriteEffects.None, 1f);
-        batch.Draw(_enemyTexture, enemyTextureRect, null, Color.White, 0f, Vector2.Zero, SpriteEffects.None, 1f);
+        var text_position = new Vector2(margin, margin);
+        var text_color = new Color(154, 139, 141);
+        batch.DrawString(_font, text, text_position, text_color, 0, Vector2.Zero, 1f, SpriteEffects.None, 1f);
 
-        text = "Level " + _game._gameScreen._map.mapLevel.ToString();
-        stringSize = _font.MeasureString(text);
-        batch.DrawString(_font, text, new Vector2(viewportWidth / 2 - stringSize.X / 2, margin), new(170, 54, 54), 0, Vector2.Zero, 1f, SpriteEffects.None, 1f);
+        // Drawing Skull Icons
+        var n_skulls = Math.Min(max_skulls, _data.Kills);
+
+        if (n_skulls == 0)  // if player has no kills yet, draw grayed-out skull
+        {
+            var skull_pos = new Rectangle(margin + (int)stringSize.X, margin, skull_size, skull_size);
+            var skull_color = new Color(100, 100, 100);
+            batch.Draw(_skull, skull_pos, null, skull_color, 0f, Vector2.Zero, SpriteEffects.None, 1f);
+        }
+        else
+        {
+            for (var i = 0; i < n_skulls; i++)
+            {
+                var skull_pos = new Rectangle(2 * margin + (int)stringSize.X + (i * 5 * skull_size / 8), margin, skull_size, skull_size);
+                var skull_color = Color.White;
+                var skull_sprite = _skull;
+                if (i % 3 == 1)
+                {
+                    skull_sprite = _skull_1;
+                }
+                else if (i % 3 == 2)
+                {
+                    skull_sprite = _skull_2;
+                }
+                batch.Draw(skull_sprite, skull_pos, null, skull_color, 0f, Vector2.Zero, SpriteEffects.None, 1f);
+            }
+        }
     }
 }
