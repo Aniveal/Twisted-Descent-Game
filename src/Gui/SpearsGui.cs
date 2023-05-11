22453cabddb2ea29a997ab29793a27356845f5e7
Dave <daviden@student.ethz.ch>
2023-05-11 19:31:23 +0200

    Add controls hint for spear GUI

--- src/Gui/SpearsGui.cs
+++ src/Gui/SpearsGui.cs
@@ -2,6 +2,7 @@ using Meridian2.Columns;
 using Meridian2.GameElements;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Meridian2.Gui; 
 
@@ -36,6 +37,11 @@ public class SpearsGui : DrawableGameElement {
     private Texture2D _wooden_spears_1;
     private Texture2D _wooden_spears_2;
     private Texture2D _wooden_spears_3;
+    
+    private Texture2D _controls_lb;
+    private Texture2D _controls_rb;
+    private Texture2D _controls_x;
+    private Texture2D _controls_x_pressed;
 
 
     public SpearsGui(RopeGame game, GameData data, SpearsController controller) {
@@ -56,6 +62,11 @@ public class SpearsGui : DrawableGameElement {
         _wooden_spears_1 = _game.Content.Load<Texture2D>("Sprites/UI/wooden_spears_1");
         _wooden_spears_2 = _game.Content.Load<Texture2D>("Sprites/UI/wooden_spears_2");
         _wooden_spears_3 = _game.Content.Load<Texture2D>("Sprites/UI/wooden_spears_3");
+        
+        _controls_lb = _game.Content.Load<Texture2D>("Sprites/Controller/Buttons/Shoulder/button_xbox_digital_bumper_dark_1");
+        _controls_rb = _game.Content.Load<Texture2D>("Sprites/Controller/Buttons/Shoulder/button_xbox_digital_bumper_dark_2");
+        _controls_x = _game.Content.Load<Texture2D>("Sprites/Controller/Buttons/ABXY/button_xbox_digital_x_1");
+        _controls_x_pressed = _game.Content.Load<Texture2D>("Sprites/Controller/Buttons/ABXY/button_xbox_digital_x_2");
 
         _font = _game.Content.Load<SpriteFont>("damn_ui");
     }
@@ -142,5 +153,21 @@ public class SpearsGui : DrawableGameElement {
             batch.DrawString(_font, wooden_spears_text, new Vector2(pos_1.X, pos_1.Y), passive_font_color, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0.95f);
         }
 
+        // Draw controls
+        var controlButton = _controls_x;
+        if (GamePad.GetState(PlayerIndex.One).Buttons.X == ButtonState.Pressed) {
+            controlButton = _controls_x_pressed;
+        }
+
+        var controlsSize = 48;
+        var controlYMargin = 45;
+        var controlRect = new Rectangle(pos_2.X + (int)((pos_2.Width - controlsSize) / 2f), pos_2.Y - controlYMargin, controlsSize, controlsSize);
+        batch.Draw(controlButton, controlRect, null, Color.White, 0f, Vector2.Zero, SpriteEffects.None, 1f);
+        
+        var lbRect = new Rectangle(pos_1.X + (int)((pos_1.Width - controlsSize) / 2f), pos_1.Y - controlYMargin, controlsSize, controlsSize);
+        batch.Draw(_controls_lb, lbRect, null, Color.White, 0f, Vector2.Zero, SpriteEffects.None, 1f);
+        
+        var rbRect = new Rectangle(pos_3.X + (int)((pos_3.Width - controlsSize) / 2f), pos_3.Y - controlYMargin, controlsSize, controlsSize);
+        batch.Draw(_controls_rb, rbRect, null, Color.White, 0f, Vector2.Zero, SpriteEffects.None, 1f);
     }
 }
