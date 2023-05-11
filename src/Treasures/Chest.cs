a29569eb2ed955ecdb698bc4fc248006bcec2fbb
David Enderlin <david.enderlin@student.ethz.ch>
2023-05-11 16:40:35 +0200

    Add WIP chest loot animation

--- src/Treasures/Chest.cs
+++ src/Treasures/Chest.cs
@@ -15,11 +15,17 @@ public class Chest : IDrawableObject {
     public Vector2 Pos;
     protected Texture2D TextureClosed;
     protected Texture2D TextureOpen;
+    protected Texture2D LootTexture;
     protected float Width = 1.5f;
     protected float Height = 1f;
     protected float spriteWidth = 1.8f;
+    protected float spriteHeight;
     protected World World;
-
+    
+    protected Texture2D TextureAnimation;
+    protected double AnimationStart;
+    protected double FadeEnd;
+    protected double FadeDuration = 1000;
 
     //TODO: modify usage of width and height in Body and draw to fit to proportions of future sprite
     public Chest(RopeGame game, World world, Vector2 position) {
@@ -32,8 +38,11 @@ public class Chest : IDrawableObject {
     }
 
     public void LoadContent() {
-        TextureClosed = Game.Content.Load<Texture2D>("Sprites/Chest/chest_closed");
-        TextureOpen = Game.Content.Load<Texture2D>("Sprites/Chest/chest_open");
+        TextureClosed = Game.Content.Load<Texture2D>("Sprites/Chest/chest_closed_updated");
+        TextureOpen = Game.Content.Load<Texture2D>("Sprites/Chest/chest_open_updated");
+        TextureAnimation = Game.Content.Load<Texture2D>("Sprites/Chest/chest_opening");
+        
+        spriteHeight = (TextureClosed.Height / (float)TextureClosed.Width) * spriteWidth;
     }
 
     //Overwrite in subclasses
@@ -42,15 +51,43 @@ public class Chest : IDrawableObject {
         
     }
 
+    protected void DrawLoot(GameTime gameTime, SpriteBatch batch, Camera camera) {
+        if (LootTexture != null) {
+            
+        }
+    }
+    
     public void Draw(GameTime gameTime, SpriteBatch batch, Camera camera) {
         var dstRec = new Rectangle();
-        dstRec = camera.getSpriteRectangle(Pos.X - spriteWidth*0.5f, Pos.Y+Height*0.88f, spriteWidth, 1.31f*spriteWidth);
-        if (Open)
-            batch.Draw(TextureOpen, dstRec, null, Color.White, 0f, Vector2.Zero, SpriteEffects.None,
-                camera.getLayerDepth(dstRec.Y + dstRec.Height));
-        else
+        dstRec = camera.getSpriteRectangle(Pos.X - spriteWidth*0.5f, Pos.Y+Height*0.88f, spriteWidth, spriteHeight);
+        if (Open) {
+            if (AnimationStart == 0)
+                AnimationStart = gameTime.TotalGameTime.TotalMilliseconds;
+
+            var animFrame = (int)((gameTime.TotalGameTime.TotalMilliseconds - AnimationStart) / 100f);
+            if (animFrame < 5) {
+                var animRect = new Rectangle(animFrame * TextureOpen.Width, 0, TextureOpen.Width, TextureOpen.Height);
+                batch.Draw(TextureAnimation, dstRec, animRect, Color.White, 0f, Vector2.Zero, SpriteEffects.None,
+                    camera.getLayerDepth(dstRec.Y + dstRec.Height));
+            } else {
+                batch.Draw(TextureOpen, dstRec, null, Color.White, 0f, Vector2.Zero, SpriteEffects.None,
+                    camera.getLayerDepth(dstRec.Y + dstRec.Height));
+
+                if (FadeEnd == 0)
+                    FadeEnd = gameTime.TotalGameTime.TotalMilliseconds + FadeDuration;
+
+                var opacity = (FadeEnd - gameTime.TotalGameTime.TotalMilliseconds) / FadeDuration;
+
+                if (opacity > 0) {
+                    var animRect = new Rectangle(4 * TextureOpen.Width, 0, TextureOpen.Width, TextureOpen.Height);
+                    batch.Draw(TextureAnimation, dstRec, animRect, new Color(255, 255, 255, (float)opacity), 0f, Vector2.Zero, SpriteEffects.None,
+                        camera.getLayerDepth(dstRec.Y + dstRec.Height) + 0.01f);
+                }
+            }
+        } else {
             batch.Draw(TextureClosed, dstRec, null, Color.White, 0f, Vector2.Zero, SpriteEffects.None,
                 camera.getLayerDepth(dstRec.Y + dstRec.Height));
+        }
     }
 
     protected bool OnCollision(Fixture sender, Fixture other, Contact contact) {
@@ -64,7 +101,6 @@ public class Chest : IDrawableObject {
         if (collider.Tag == null) return true;
         ///player collision
         if (collider.Tag is Player) {
-            //TODO: play opening animation?
             Loot();
             Open = true;
         }
