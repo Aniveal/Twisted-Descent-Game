b266985ab77d7061b773caf41f7a5a8bd20ef545
bglaus <bglaus@student.ethz.ch>
2023-05-15 14:54:49 +0200

    reworking controls screen

--- src/Screens/ControlScreen.cs
+++ src/Screens/ControlScreen.cs
@@ -14,28 +14,55 @@ public class ControlScreen : Screen {
     private SpriteBatch _spriteBatch;
     public GraphicsDeviceManager Graphics;
     private Texture2D _playerModel;
-    private Texture2D _enemyModel;
 
     private Texture2D _bg;
-    private Texture2D _menu_img;
-    private Texture2D _menu_title;
+
+    private Texture2D LB;
+    private Texture2D RB;
+    private Texture2D X;
+    private Texture2D RT;
+    private Texture2D A;
+    private Texture2D Left_Stick;
+
+    private Texture2D Q;
+    private Texture2D E;
+    private Texture2D R;
+    private Texture2D P;
+    private Texture2D Space;
+    private Texture2D WASD;
+    private Texture2D Arrow_Keys;
 
     public Boolean gameLoaded;
     double timer;
     private SpriteFont font;
+    private Color font_color;
 
     private int w;
     private int h;
 
     public ControlScreen(RopeGame game, ContentManager content) : base(game)
     {
-        font = content.Load<SpriteFont>("Fonts/Arial40");
+        font = content.Load<SpriteFont>("Fonts/control_screen_text");
+        font_color = new Color(154, 134, 129);
+
         _playerModel = content.Load<Texture2D>("Sprites/Theseus/model");
-        _enemyModel = content.Load<Texture2D>("Sprites/Enemies/Minotaur/minotaur_idle_flip");
 
         _bg = content.Load<Texture2D>("Sprites/UI/menu_background");
-        _menu_img = content.Load<Texture2D>("Sprites/UI/menu_img");
-        _menu_title = content.Load<Texture2D>("Sprites/UI/menu_title");
+
+        LB = content.Load<Texture2D>("Sprites/Controller/LB");
+        RB = content.Load<Texture2D>("Sprites/Controller/RB");
+        X = content.Load<Texture2D>("Sprites/Controller/X");
+        RT = content.Load<Texture2D>("Sprites/Controller/RT");
+        A = content.Load<Texture2D>("Sprites/Controller/A");
+        Left_Stick = content.Load<Texture2D>("Sprites/Controller/Left_Stick");
+
+        Q = content.Load<Texture2D>("Sprites/Controller/Q");
+        E = content.Load<Texture2D>("Sprites/Controller/E");
+        R = content.Load<Texture2D>("Sprites/Controller/R");
+        P = content.Load<Texture2D>("Sprites/Controller/P");
+        Space = content.Load<Texture2D>("Sprites/Controller/Space");
+        WASD = content.Load<Texture2D>("Sprites/Controller/WASD");
+        Arrow_Keys = content.Load<Texture2D>("Sprites/Controller/Arrow_Keys");
 
         w = game.GraphicsDevice.PresentationParameters.BackBufferWidth;
         h = game.GraphicsDevice.PresentationParameters.BackBufferHeight;
@@ -48,39 +75,41 @@ public class ControlScreen : Screen {
     {
         spriteBatch.Begin();
 
+        // Draw Background:
         spriteBatch.Draw(_bg, new Rectangle(0, 0, w, h), Color.White);
-        int title_width = Math.Min(w / 2 - 150, 2 * (h - 200) / 3);
-        int title_height = 2 * title_width / 3;
-        //spriteBatch.Draw(_menu_title, new Rectangle(100, h - title_height - 100, title_width, title_height), Color.White);
-        int img_width = Math.Min(w / 2 - 150, (h - 200) / 2);
-        int img_height = 2 * img_width;
-        //spriteBatch.Draw(_menu_img, new Rectangle(w - 100 - img_width, h - 100 - img_height, img_width, img_height), Color.White);
 
-        spriteBatch.DrawString(font, "In this game you are playing as Theseus (", new Vector2(w / 16, h / 18), Color.White);
-        spriteBatch.Draw(_playerModel, new Rectangle(w / 16 + 965, h / 18, w / 25, w / 25), Color.White);
-        spriteBatch.DrawString(font, ")", new Vector2(w / 16 + 1020, h / 18), Color.White);
+        // Write Text:
+        int font_height = (int)font.MeasureString("Test").Y;
+        int horizontal_margin = w / 16;
+        int vertical_margin = h / 16;
 
-        spriteBatch.DrawString(font, "Your goal is to kill as many Enemies as you can and ", new Vector2(w / 16, h / 9), Color.White);
-        spriteBatch.DrawString(font, "proceed to the Exit Point ", new Vector2(w / 16, h / 6), Color.White);
+        String intro_text_1 = "In this game you are playing as Theseus (";
 
+        spriteBatch.DrawString(font, intro_text_1, new Vector2(horizontal_margin, vertical_margin), font_color);
+        spriteBatch.Draw(_playerModel, new Rectangle(horizontal_margin + (int)font.MeasureString(intro_text_1).X, vertical_margin, 5 * font_height / 9, font_height), Color.White);
+        spriteBatch.DrawString(font, ")", new Vector2(horizontal_margin + (int)font.MeasureString(intro_text_1).X + 5 * font_height / 9, vertical_margin), font_color);
 
-        spriteBatch.DrawString(font, "Player Movement : ", new Vector2(w / 16, h / 9 * 3), Color.White);
-        spriteBatch.DrawString(font, "WASD / left stick", new Vector2(w / 16 * 12, h / 9 * 3), Color.White);
+        spriteBatch.DrawString(font, "Your goal is to kill as many Enemies as you can and ", new Vector2(horizontal_margin, vertical_margin + font_height), font_color);
+        spriteBatch.DrawString(font, "proceed to the Exit Point.", new Vector2(horizontal_margin, vertical_margin + 2 * font_height), font_color);
+        
 
-        spriteBatch.DrawString(font, "Pull the Rope : ", new Vector2(w / 16, h / 9 * 4), Color.White);
-        spriteBatch.DrawString(font, "P / RT", new Vector2(w / 16 * 12, h / 9 * 4), Color.White);
+        spriteBatch.DrawString(font, "Player Movement : ", new Vector2(horizontal_margin, vertical_margin + 4 * font_height), font_color);
+        spriteBatch.DrawString(font, "WASD / left stick", new Vector2(8 * horizontal_margin, vertical_margin + 4 * font_height), font_color);
 
-        spriteBatch.DrawString(font, "Dash : ", new Vector2(w / 16, h / 9 * 5), Color.White);
-        spriteBatch.DrawString(font, "Space / A", new Vector2(w / 16 * 12, h / 9 * 5), Color.White);
+        spriteBatch.DrawString(font, "Pull the Rope : ", new Vector2(horizontal_margin, vertical_margin + 5 * font_height), font_color);
+        spriteBatch.DrawString(font, "P / RT", new Vector2(8 * horizontal_margin, vertical_margin + 5 * font_height), font_color);
 
-        spriteBatch.DrawString(font, "Change between Spears : ", new Vector2(w / 16, h / 9 * 6), Color.White);
-        spriteBatch.DrawString(font, "Q, E / LB, RB", new Vector2(w / 16 * 12, h / 9 * 6), Color.White);
+        spriteBatch.DrawString(font, "Dash : ", new Vector2(horizontal_margin, vertical_margin + 6 * font_height), font_color);
+        spriteBatch.DrawString(font, "Space / A", new Vector2(8 * horizontal_margin, vertical_margin + 6 * font_height), font_color);
 
-        spriteBatch.DrawString(font, "Place a Spear: ", new Vector2(w / 16, h / 9 * 7), Color.White);
-        spriteBatch.DrawString(font, "R / X", new Vector2(w / 16 * 12, h / 9 * 7), Color.White);
+        spriteBatch.DrawString(font, "Change between Spears : ", new Vector2(horizontal_margin, vertical_margin + 7 * font_height), font_color);
+        spriteBatch.DrawString(font, "Q, E / LB, RB", new Vector2(8 * horizontal_margin, vertical_margin + 7 * font_height), font_color);
 
-        spriteBatch.DrawString(font, "Pause/Back to Menu: ", new Vector2(w / 16, h / 9 * 8), Color.White);
-        spriteBatch.DrawString(font, "Esc / start", new Vector2(w / 16 * 12, h / 9 * 8), Color.White);
+        spriteBatch.DrawString(font, "Place a Spear: ", new Vector2(horizontal_margin, vertical_margin + 8 * font_height), font_color);
+        spriteBatch.DrawString(font, "R / X", new Vector2(8 * horizontal_margin, vertical_margin + 8 * font_height), font_color);
+
+        spriteBatch.DrawString(font, "Pause/Back to Menu: ", new Vector2(horizontal_margin, vertical_margin + 9 * font_height), font_color);
+        spriteBatch.DrawString(font, "Esc / start", new Vector2(8 * horizontal_margin, vertical_margin + 9 * font_height), font_color);
 
 
         spriteBatch.End();
