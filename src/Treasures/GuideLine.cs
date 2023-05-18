f79fabca6231669627c787380ee1e15a0d78bffa
Jules Bachmann <julesb@student.ethz.ch>
2023-05-18 13:25:03 +0200

    lines texture now generated once only

--- src/Treasures/GuideLine.cs
+++ src/Treasures/GuideLine.cs
@@ -16,6 +16,8 @@ public class GuideLine : IDrawableObject {
     // private float rotation;
     // private Vector2 o;
     private const int thickness = 4;
+    private Texture2D texture;
+    private bool textureInitialized = false;
 
     public GuideLine(RopeGame game, Vector2 start, Vector2 end) {
         a = start;
@@ -39,19 +41,19 @@ public class GuideLine : IDrawableObject {
         Vector2 aScreen = camera.getScreenPoint(a);
         Vector2 bScreen = camera.getScreenPoint(b);
 
-        int distance = (int)Vector2.Distance(aScreen, bScreen);
-        Texture2D line  = new Texture2D(_game.GraphicsDevice, distance, thickness);
-        Color[] data = new Color[distance * thickness];
-        for (int i = 0; i < data.Length; i++) {
-            data[i] = Color.Blue;
+        if (!textureInitialized) {
+            int distance = (int)Vector2.Distance(aScreen, bScreen);
+            texture  = new Texture2D(_game.GraphicsDevice, distance, thickness);
+            Color[] data = new Color[distance * thickness];
+            for (int i = 0; i < data.Length; i++) {
+                data[i] = Color.Blue;
+            }
+            texture.SetData(data);
+            textureInitialized = true;
         }
-        line.SetData(data);
-
+        
         Vector2 o = new Vector2(0, thickness / 2);
         float rot = (float)Math.Atan2(bScreen.Y - aScreen.Y, bScreen.X - aScreen.X);
-        batch.Draw(line, aScreen, null, Color.White, rot, o, 1.0f, SpriteEffects.None, 0.245f);
-        if (Keyboard.GetState().IsKeyDown(Keys.H)) {
-            Console.WriteLine("a: "+a.ToString()+", aScreen: "+aScreen.ToString()+ ", playerpos: "+_game._gameScreen.TheseusManager.Player.Body.Position.ToString());
-        }
+        batch.Draw(texture, aScreen, null, Color.White, rot, o, 1.0f, SpriteEffects.None, 0.245f);
     }
 }
